94aa1108ff8faa4ac6e55dfa81d58924636ae803
JuanJoseFamiliaPolanco <familiajuan05@gmail.com>
2025-02-15 18:05:43 -0400

    prueba unitaria y configuración de CI

---
+++ src/index.js
@@ -0,0 +1,5 @@
+function saludo() {
+  return 'Hola, mundo!'; //Juan Jose Familia 2023-0218
+}
+
+module.exports = saludo;
---
+++ src/prueba.test.js
@@ -0,0 +1,5 @@
+const saludo = require('./index');
+//Juan Jose Familia Polanco 2023-0218
+test('Funcionando, Juan Jose Familia polanco 20230218', () => {
+  expect(saludo()).toBe('Hola, mundo!');
+});
